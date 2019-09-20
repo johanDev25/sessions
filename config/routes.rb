497dbb1dfa9ca15fb737d9f1213fac537a8c4683
Johan <johan225@hotmail.es>
2019-09-20 14:56:16 -0500

    Agrega rutas post

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,7 @@ Rails.application.routes.draw do
   delete 'logout', to: 'sessions#destroy'
 
   resources :users, only: [:new, :create]
+
+  resources :posts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
