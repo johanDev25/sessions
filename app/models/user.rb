d8b65bff0bfff284265dc39828f77b95d230a0e1
Johan <johan225@hotmail.es>
2019-09-20 14:17:32 -0500

    Agrega registro y login de usuarios

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+	validates :email, uniqueness: true, format: { with: /@/,
+    message: "Debe contener un arroba" }
+	validates :password_digest, presence: true, on: :create
+	validates :password_digest, length: { in: 6..20 }, allow_nil: true
+	validates :name, presence: true
+
+	has_secure_password
+	has_secure_password validation: false
+end
